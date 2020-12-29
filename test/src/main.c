c924062b0991d0151bc82668dc9608c8631997d3
popgoesme700 <17504205+popgoesme700@users.noreply.github.com>
2020-12-29 09:14:53 -0700

    Slightly more test-y test unit program.

--- test/src/main.c
+++ test/src/main.c
@@ -5,11 +5,44 @@
 #include <popnbt.h>
 
 int main(const int argc,const char *argv[]){
-	const char data[]= {0x0a,0x0b,0x00,'h','e','l','l','o',' ','w','o','r','l','d',0x08,0x04,0x00,'n','a','m','e',0x09,0x00,'B','a','n','a','n','r','a','m','a',0x00,0x0a,0x0d,0x00,'g','o','o','d','b','y','e',' ','w','o','r','l','d',0x08,0x04,0x00,'n','a','m','e',0x09,0x00,'A','p','p','l','e','r','a','m','a',0x00};
+	const char data[]= {0x0a,0x0b,0x00,'h','e','l','l','o',' ','w','o','r','l','d',0x03,0x04,0x00,'n','a','m','e',0x0f,0x05,0xf0,0x50,0x00};
 	poplibs_popnbtparser parser;
 	poplibs_popnbttoken_t tokens[64];
 	unsigned made;
 	poplibs_popnbtparser_init(&parser,0);
-	made= poplibs_popnbtparser_parse(&parser,data,68,tokens,64);
+	made= poplibs_popnbtparser_parse(&parser,data,25,tokens,64);
+	printf("Made %u tokens.\n",made);
+	if(made>=2){
+		poplibs_popnbttoken_t *ourtok= &tokens[1];
+		if(ourtok->type>=poplibs_popnbttype_byte && ourtok->type<=poplibs_popnbttype_long){
+			char bnum;
+			short snum;
+			int inum;
+			switch(ourtok->type){
+				case poplibs_popnbttype_byte:
+					bnum= ourtok->value.lnum;
+					puts("Compound token 0 is a byte.");
+					printf("Compound token 0 value: 0x%.2X\n",bnum);
+					break;
+				case poplibs_popnbttype_short:
+					snum= ourtok->value.lnum;
+					puts("Compound token 0 is a short.");
+					printf("Compound token 0 value: 0x%.2X\n",snum);
+					break;
+				case poplibs_popnbttype_int:
+					inum= ourtok->value.lnum;
+					puts("Compound token 0 is a int.");
+					printf("Compound token 0 value: 0x%.2X\n",inum);
+					break;
+				case poplibs_popnbttype_long:
+					puts("Compound token 0 is a long int.");
+					printf("Compound token 0 value: 0x%.2lX\n",ourtok->value.lnum);
+					break;
+				default:
+					puts("Compound token 0 has somehow slipped between the cracks?");
+					break;
+			}
+		}
+	}
 	return 0;
 }
